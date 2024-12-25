1b6e64b030e68b4854555264aa85861661101ac8
ash <ahnsh2748@gmail.com>
2024-12-25 11:33:53 +0900

    버블정렬 수정: 정렬수행되어있으면 패스

--- 2. Algorithm/Sort/BubbleSort/BubbleSort.c
+++ 2. Algorithm/Sort/BubbleSort/BubbleSort.c
@@ -4,20 +4,30 @@ void BubbleSort(int DataSet[], int Length){ // 5
     int i=0; 
     int j=0; 
     int temp =0;
+    int passCount = 0;
 
     for (i=0; i<Length-1; i++) { 
+        passCount=0;
+
         for(j=0; j<Length-(i+1); j++) {
             if (DataSet[j] > DataSet[j+1]) {
+                printf("%d\n", i);
                 temp = DataSet[j+1];
                 DataSet[j+1] = DataSet[j];
                 DataSet[j] = temp;
+            } else {
+                passCount++;
             }
+        }
+
+        if (passCount == (Length-(i+1))) {
+            break;
         }
     }
 }
 
 int main(void) {
-    int DataSet[] = {6,4,2,3,1,5};
+    int DataSet[] = {2,1,3,4,5,6};
     int Length = sizeof DataSet/ sizeof DataSet[0];
     int i=0;
 
@@ -26,7 +36,7 @@ int main(void) {
     BubbleSort(DataSet, Length);
 
     for(i=0; i<Length; i++) {
-        printf("%d", DataSet[i]); //정렬 후 데이터 집합 출력
+        printf("%d ", DataSet[i]); //정렬 후 데이터 집합 출력
     }
 
     printf("\n");
