9aa4ef6e1de1a65e386968a329e7c78144ed9539
ash <ahnsh2748@gmail.com>
2024-12-28 11:16:48 +0900

    이진탐색트리 학습

--- 2. Algorithm/Search/BinarySearchTree/BinarySearchTree.c
+++ 2. Algorithm/Search/BinarySearchTree/BinarySearchTree.c
@@ -56,3 +56,83 @@ BSTNode* BST_SearchMinNode(BSTNode* Tree) {
 	}
 }
 
+void BST_InsertNode(BSTNode* Tree, BSTNode* Child) {
+	if (Tree->Data < Child->Data) {
+		if (Tree->Right == NULL) {
+			Tree->Right = Child;
+		} else {
+			BST_InsertNode(Tree->Right, Child);
+		}
+	} else if (Tree->Data > Child->Data) {
+		if (Tree->Left == NULL) {
+			Tree->Left = Child;
+		} else {
+			BST_InsertNode(Tree->Left, Child);
+		}
+	}
+}
+
+BSTNode* BST_RemoveNode(BSTNode* Tree, BSTNode* Parent, ElementType Target) {
+	BSTNode* Removed = NULL;
+
+	if (Tree == NULL) {
+		return NULL;
+	}
+
+	if (Tree->Data > Target) {
+		Removed = BST_RemoveNode(Tree->Left, Tree, Target);
+	} else if (Tree->Data < Target) {
+		Removed = BST_RemoveNode(Tree->Right, Tree, Target);
+	} else {
+		Removed = Tree;
+
+		//잎 노드인 경우 바로 삭제
+		if (Tree->Left == NULL && Tree->Right == NULL) {
+			//제거하려는 Target이 왼쪽 노드라면
+			if (Parent->Left == Tree) {
+				Parent->Left = NULL;
+			} else {
+				Parent->Right = NULL;
+			}
+		} else {
+			//자식이 양쪽 다 있는 경우
+			if (Tree->Left != NULL && Tree->Right != NULL) {
+				//최소값 노드를 찾아 제거한 뒤 현재 노드에 위치시킴
+				BSTNode* MinNode = BST_SearchMinNode(Tree);
+				MinNode = BST_RemoveNode(Tree, NULL, MinNode->Data);
+				Tree->Data = MinNode->Data;
+			} else {
+				//자식이 하나만 있는 경우
+				BSTNode* Temp = NULL;
+
+				if (Tree->Left != NULL) {
+					Temp = Tree->Left;
+				} else {
+					Temp = Tree->Right;
+				}
+
+				if (Parent->Left == Tree) {
+					Parent->Left = Temp;
+				} else {
+					Parent->Right = Temp;
+				}
+			}
+		}
+	}
+
+	return Removed;
+}
+
+void BST_InorderPrintTree(BSTNode* Node) {
+	if (Node == NULL) {
+		return;
+	}
+
+	//왼쪽 하위 출력
+	BST_InorderPrintTree(Node->Left);
+
+	printf("%d ", Node->Data);
+
+	//오른쪽 하위 출력
+	BST_InorderPrintTree(Node->Right);
+}
