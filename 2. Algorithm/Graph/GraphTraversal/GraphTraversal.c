3296e1662026b657e22538c75d0c8b2d1d65b6d4
안성현 <ahnsh2748@gmail.com>
2025-01-15 21:20:17 +0900

    DFS,BFS복습

--- 2. Algorithm/Graph/GraphTraversal/GraphTraversal.c
+++ 2. Algorithm/Graph/GraphTraversal/GraphTraversal.c
@@ -1,48 +1,47 @@
 #include "GraphTraversal.h"
 
 void DFS(Vertex* V) {
-    Edge* E = NULL;
+	Edge* E = NULL;
 
-    printf("%d ", V->Data);
+	printf("%d ", V->Data);
 
-    V->Visited = Visited;
+	V->Visited = Visited;
+	
+	E = V->AdjacencyList;
 
-    E = V->AdjacencyList;
+	while (E != NULL) {
+		if (E->Target != NULL && E->Target->Visited == NotVisited) {
+			DFS(E->Target);
+		}
 
-    //E의 NEXT가 있으면 DFS호출
-    while (E != NULL) {
-        if (E->Target != NULL && E->Target->Visited == NotVisited) {
-            DFS(E->Target);
-        }
-
-        E = E->Next;
-    }
+		E = E->Next;
+	}
 }
 
 void BFS(Vertex* V, LinkedQueue* Queue) {
-    Edge* E = NULL;
+	Edge* E = NULL;
 
-    printf("%d ", V->Data);
-    V->Visited = Visited;
+	printf("%d v", V->Data);
+	V->Visited = Visited;
 
-    //큐에 노드 삽입
-    LQ_Enqueue(&Queue, LQ_CreateNode(V));
+	//큐에 노드 삽입
+	LQ_Enqueue(&Queue, LQ_CreateNode(V));
 
-    while(!LQ_IsEmpty(Queue)){
-        Node* Popped = LQ_Dequeue(&Queue);
-        V = Popped->Data;
-        E = V->AdjacencyList;
+	while (!LQ_IsEmpty(Queue)) {
+		Node* Popped = LQ_Dequeue(&Queue);
+		V = Popped->Data;
+		E = V->AdjacencyList;
 
-        while(E != NULL) {
-            V = E->Target;
+		while (E != NULL) {
+			V = E->Target;
+	
+			if (V != NULL && V->Visited == NotVisited) {
+				printf("%d ", V->Data);
+				V->Visited = Visited;
+				LQ_Enqueue(&queue, LQ_CreateNode(V));
+			}
 
-            if (V != NULL && V->Visited == NotVisited) {
-                printf("%d ", V->Data);
-                V->Visited = Visited;
-                LQ_Enqueue(&Queue, LQ_CreateNode(V));
-            }
-
-            E = E->Next;
-        }
-    }
+			E = E->Next;
+		}
+	}
 }
