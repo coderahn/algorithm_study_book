01172bc339b17c86ed549cb2c57f67214a01de1f
ash <ahnsh2748@gmail.com>
2025-01-18 10:05:54 +0900

    퀵정렬 복습!

--- 2. Algorithm/Sort/QuickSort/QuickSort.c
+++ 2. Algorithm/Sort/QuickSort/QuickSort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void Swap(int* A, int* B){
+void Swap(int* A, int* B) {
     int Temp = *A;
     *A = *B;
     *B = Temp;
@@ -13,32 +13,33 @@ int Partition(int DataSet[], int Left, int Right) {
     Left++;
 
     while (Left < Right) {
-        while(Pivot >= DataSet[Left]) {
+        while(DataSet[Left] <= Pivot) {
             Left++;
         }
 
-        while(Pivot < DataSet[Right]) {
+        while(DataSet[Right] > Pivot) {
             Right--;
         }
 
         if (Left < Right) {
             Swap(&DataSet[Left], &DataSet[Right]);
-        } else {
+        }else {
             break;
         }
     }
 
+    //Right가 Left보다 작아지는 시점. Right와 Pivot 위치 변경
     Swap(&DataSet[First], &DataSet[Right]);
 
     return Right;
 }
 
 void QuickSort(int DataSet[], int Left, int Right) {
-    if(Left < Right) {
-        int index = Partition(DataSet, Left, Right);
+    if (Left < Right) {
+        int Index = Partition(DataSet, Left, Right);
 
-        QuickSort(DataSet, Left, index-1);
-        QuickSort(DataSet, index+1, Right);
+        QuickSort(DataSet, Left, Index-1);
+        QuickSort(DataSet, Index+1, Right);       
     }
 }
 
@@ -49,7 +50,7 @@ int main(void) {
 
     QuickSort(DataSet, 0, Length-1);
 
-    for (i=0; i < Length; i++) {
+    for(i = 0 ; i<Length; i++) {
         printf("%d ", DataSet[i]);
     }
 
